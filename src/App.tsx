24bd1865f4f7382476a40f25dd82ca32390c384c
Pedro-Alcantara-M <pedrinrn@gmail.com>
2025-06-11 17:30:09 -0300

    updated app name

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>Rspack + React + TypeScript</h1>
+			<h1>MFE - 1</h1>
 			<div className="card">
 				<Button />
 			</div>
